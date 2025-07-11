3ed9f8c6e71ce4b4e6319722d0d17eccc0d559b1
hit patel <Hitdhami4@gmail.com>
2025-07-11 13:13:30 +0530

    add request validation

--- internal/http/handlers/student/students.go
+++ internal/http/handlers/student/students.go
@@ -3,10 +3,12 @@ package student
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"log/slog"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
 	"github.com/hitdhami0438/rest-api-go/internal/types"
 	"github.com/hitdhami0438/rest-api-go/internal/utils/response"
 )
@@ -18,7 +20,21 @@ func New() http.HandlerFunc {
 
 		err := json.NewDecoder(r.Body).Decode(&student)
 		if errors.Is(err, io.EOF) {
-			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty body")))
+			return
+		}
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadGateway, response.GeneralError(err))
+			return
+		}
+
+		// request validation
+		if err := validator.New().Struct(student); err != nil {
+
+			validatErrs := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validatErrs))
+			
 			return
 		}
 
